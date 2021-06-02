86de88980d3dd0259a5eaa15e0ba8df1ebc06f6f
Abanoub Iskander <abanoub@iskander.dev>
2021-06-02 11:31:56 +0100

    Initial Commit

---
+++ BrainTrainRPS/BrainTrainRPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainRPSApp.swift
+//  BrainTrainRPS
+//
+//  Created by Abanoub Iskander on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainRPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
